a01f0ede058fdd175cd29c6afeb2c96a76e8925a
id10tothe9 <id10tothe9@googlemail.com>
2024-01-25 22:16:27 +0000

    add exercise solutions

--- filter.js
+++ filter.js
@@ -21,6 +21,12 @@ const people = [
   },
 ];
 
+let over21 = people.filter(person => person.age < 21).map(p => p.name);
+// console.log(over21);
+
+const paul = people.filter(p => p.name === 'Paul')[0];
+// console.log(paul);
+
 
 // Complex Filtering
 const students = [
@@ -55,3 +61,32 @@ const students = [
     ]
   },
 ];
+
+// let interviewees = students.filter(s => (s.skills[0].yrsExperience >=5 || s.skills[1].yrsExperience >=5 || s.skills[2].yrsExperience >=5));
+// console.log(interviewees);
+
+// const candidates = students.filter(student => {
+//   const listOfSkillsWithEnoughExperience = student.skills.filter(skill => skill.yrsExperience >= 5);
+//   return (listOfSkillsWithEnoughExperience.length > 0);
+// });
+// console.log(candidates);
+
+ const has5yrsExp = skill => skill.yrsExperience >= 5;
+ const highSkills = student => student.skills.filter(has5yrsExp).length > 0;
+ const candidates = students.filter(highSkills).map(s => s.name);
+ console.log(candidates);
+
+
+// let candidates = students.filter(s => {
+//   let enoughX = s.skills.filter(x => x.yrsExperience >= 5);
+//   return enoughX.length > 0;
+// });
+// console.log(candidates);
+
+// const filterFunction = (student) => {
+//   let listOfSkills = [...student.skills];
+
+// }
+// console.log()
+// candidates = students.filter(s => filterFunction(s));
+// console.log(candidates);
